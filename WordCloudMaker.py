47429d9eed3fa22c71f72cef588a6d7e2290af06
Migmarti <mmart96@hotmail.com>
2018-05-29 20:51:19 -0700

    Filter for Word Cloud

--- WordCloudMaker.py
+++ WordCloudMaker.py
@@ -4,13 +4,14 @@ import os
 import io
 import matplotlib.pyplot as plt
 import random
+import nltk
 
 from wordcloud import WordCloud, STOPWORDS
 
 basepath = os.getcwd() + '/WebCrawlers'
 newsfolderpath = basepath + '/SortedNewsArticles'
 imagesfolderpath = basepath + '/DefaultImages'
-outputpath = os.getcwd() + '/WordClouds/'
+outputpath = basepath + '/WordClouds/'
 
 def grey_color_func(word, font_size, position, orientation, random_state=None,
                     **kwargs):
@@ -36,6 +37,25 @@ def readFile(filepath):
         text = text.encode('ascii', 'ignore')
     return text
 
+def filterWords(tokens):
+    stopwords = nltk.corpus.stopwords
+    stops = set(stopwords.words("english"))
+    filteredtokens = [word for word in tokens if word.lower() not in stops and word.isalpha()]
+    #print(filteredtokens)
+    return filteredtokens
+
+def tokenizeText(text):
+    tokens = nltk.tokenize.word_tokenize(text.lower())
+    tokens = filterWords(tokens)
+    return tokens
+
+def getNouns(text):
+    tokens = tokenizeText(text)
+    tagged = nltk.pos_tag(tokens)
+    nouns = [word for word, pos in tagged
+             if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS')]
+    return ' '.join(nouns)
+
 FoldersPaths = getFilepaths(newsfolderpath)
 ImagesPath = getImageFilepaths(imagesfolderpath)
 
@@ -46,11 +66,16 @@ for category in FoldersPaths:
     newsFiles = getFilepaths(FoldersPaths[category])
     for news in newsFiles:
         fullText += str(readFile(newsFiles[news]))
+    print("Finished reading text...")
+    fullText = getNouns(fullText)
+    print("Finished filtering text...")
     mask = np.array(Image.open(imagePath))
-    stopwords = set(STOPWORDS)
-    stopwords.add("int")
-    stopwords.add("ext")
-    wc = WordCloud(max_words=1000, mask=mask, stopwords=stopwords, margin=10,
+    #stopwords = set(STOPWORDS)
+    #stopwords.add("int")
+    #stopwords.add("ext")
+    wc = WordCloud(max_words=1000, mask=mask,
+                   #stopwords=stopwords,
+                   margin=10,
                    random_state=1).generate(fullText)
     default_colors = wc.to_array()
     plt.imshow(default_colors, interpolation="bilinear")
